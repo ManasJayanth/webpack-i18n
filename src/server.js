d34432102a43e030a7a25dcec12ee91adf785294
Manas <prometheansacrifice@gmail.com>
2018-07-24 21:41:34 +0530

    wip

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-const path = require('path');
-const fs = require('fs');
 
 module.exports = function render(locals) {
+const path = require('path');
+const fs = require('fs');
   const language = locals.path.split('/')[1]
   console.log(
     '>>>>', path.resolve(locals.pwd, `../i18n-build/index.${language}.js`),
