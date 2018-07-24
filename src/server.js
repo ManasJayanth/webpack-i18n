2b7c6ad9804a028adc552cdf022fc36d3390c5a3
Manas <prometheansacrifice@gmail.com>
2018-07-24 21:54:10 +0530

    add dist

--- src/server.js
+++ src/server.js
@@ -2,12 +2,9 @@
 module.exports = function render(locals) {
 
   const language = locals.path.split('/')[1]
-  console.log(
-    '>>>>', require(`./i18n-build/index.${language}.js`),
-  )
   return Promise.resolve(
     locals.ejsCompile(
-      require(`./i18n-build/index.${language}.js`)
+      require(`../i18n-build/index.${language}.js`)
     )
   )
   // return '<html> <a href="/foo">Foo</a>'  + locals.greet + ' from ' + locals.path + '</html>';
